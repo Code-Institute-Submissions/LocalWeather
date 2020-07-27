124d1f41ef1a319975b38947d314bcff45c6d314
Elin <elinkw@live.se>
2020-07-27 19:02:18 +0200

    making sure javascript is linked correctly

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,11 +1,1 @@
-function getWeather(event) {
-	
-
-	var city = $("#city").val();
-	if (!city) {
-		$('#displayWeather').html(`<p> Enter a valid city </p>`);
-		return;
-	}
-	
-	$('#displayWeather').html(`<p> getting data </p>`);
-}
+console.log("hej");
