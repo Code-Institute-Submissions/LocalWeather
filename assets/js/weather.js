f04414bf391d78f778decae7cf53022ac87a7924
Elin <elinkw@live.se>
2020-07-27 18:23:49 +0200

    Fixed error with closing tags index.html

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,1 +1,11 @@
-// JavaScript Document
+function getWeather(event) {
+	
+
+	var city = $("#city").val();
+	if (!city) {
+		$('#displayWeather').html(`<p> Enter a valid city </p>`);
+		return;
+	}
+	
+	$('#displayWeather').html(`<p> getting data </p>`);
+}
