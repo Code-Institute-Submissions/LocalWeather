9de244a79cf3c0230e7776013b7027e186f18e0d
Elin <elinkw@live.se>
2020-07-28 15:30:38 +0200

    Added api info to js file

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,1 +1,13 @@
-console.log("hej");
+
+//Api information from openweathermap
+const api = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const apiKey = '&appid=eb530ff6c72ca78b53c5c2a7a8d90b49';
+const units = '&units=metric';
+
+//create function that takes the input of getCity button
+function getCity(data) {
+
+  const myInput = document.getElementById('city');
+
+  this.weatherSearch(myInput.value);
+}
